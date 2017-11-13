b34ec4d9e1927b0457a0cc07f9f2d6bc405e2992
Mike Barretta <mike.barretta@gmail.com>
2017-11-13 12:01:58 -0500

    don't event remember anymore...

--- src/main/groovy/com/elastic/barretta/analytics/rosette/RosetteApiClient.groovy
+++ src/main/groovy/com/elastic/barretta/analytics/rosette/RosetteApiClient.groovy
@@ -1,8 +1,11 @@
 package com.elastic.barretta.analytics.rosette
 
+import groovy.json.JsonOutput
+import groovy.util.logging.Slf4j
 import wslite.rest.RESTClient
 import wslite.rest.RESTClientException
 
+@Slf4j
 class RosetteApiClient {
     private RESTClient client
     private Config config
@@ -15,20 +18,49 @@ class RosetteApiClient {
 
     RosetteApiClient(Config config) {
         client = new RESTClient(config.url)
+        client.httpClient.connectTimeout = 5000
         this.config = config
         if (this.config.key) {
             headers = ["X-RosetteAPI-Key": this.config.key]
         }
+        test()
     }
 
     def test() throws RESTClientException {
         client.get(path: "/ping", headers: headers)
+        log.info("able to connect to Rosette API [$config.url]")
     }
 
-    def getEntities(text) {
-        def response = client.post(path: "/entities", headers: headers) {
-            json content: text
+    List getEntities(text) {
+        def returnList = []
+        try {
+            if (text && !text.trim().isEmpty()) {
+                def response = client.post(path: "/entities", headers: headers) {
+                    json content: text
+                }
+                returnList = response.json.entities.collect()
+            }
+        } catch (e) {
+            log.error("error fetching entities [$e.cause]")
         }
-        return response.json.entities.collect()
+        return returnList
+    }
+
+    Map getSentiment(text) {
+        def returnMap = [:]
+        try {
+            if (text && !text.trim().isEmpty()) {
+                def response = client.post(path: "/sentiment", headers: headers) {
+                    json content: text
+                }
+                returnMap = response.json
+            }
+        } catch (RESTClientException e) {
+            log.error("error getting sentiment [$e.cause]")
+            if (e.response.statusCode == 400) {
+                log.error("detail [\n${JsonOutput.prettyPrint(new String(e.response.data))}\n]")
+            }
+        }
+        return returnMap
     }
 }
